df86314060866b3463c2ca07f23edc3c380a16f1
sashkka1 <84565833+sashkka1@users.noreply.github.com>
2025-05-30 09:50:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    alert('2');
+    alert('3');
 let values =[]; // 0+   1-   2x   3/  4t  5+-(min)  6+-(max)  7x/(min)  8x/(max) 
 let examples =[];
 let score = 1, mistake =0, mistakeTwo=0,timeForStatsArray=0, mistakeForStatsArray=0, examplesForStatsArray=0, totalTime=0,examplesCount=10;
@@ -1067,8 +1067,9 @@ document.addEventListener('DOMContentLoaded', () => { // первый заход
                 statsArray = [0,0,0];
             }
         }
-        console.log('stats2 - ', stats, 'daysInMonth',daysInMonth);
+        console.log('stats2 - ', stats);
         for(let i=1;i<=daysInMonth;i++){
+            console.log('i - ', i, 'stats',stats);
             stats[i]= [0,0,0];
         }; 
         console.log('stats3 - ', stats);
